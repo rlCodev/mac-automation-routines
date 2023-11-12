75df9c60312daf93ba61eb3f665b114bd4558ed4
Leon Remke <remke.lcode@gmail.com>
2023-11-12 16:09:51 +0100

    add basic startup routines

---
+++ README.md
@@ -0,0 +1,13 @@
+# Mac startup routines:
+
+Simple startup routines to inclide into apples mac os shortcuts app.
+
+## How to integrate:
+
+1. Open "Shortcuts"-App
+2. Add new shortcut
+3. Select "Scripting" filter category on the right
+4. Select "Run JavaScript for Mac automation"
+5. Paste your js code into the editor
+6. Close the editor window
+7. Test shortcut by triggering spotlight and typing in the name of your shortcut.
---
+++ UNI_startup.js
@@ -0,0 +1,106 @@
+config = {
+    "apps": [
+        {
+            "name": "Visual Studio Code",
+            "action": "your_action_here",
+            "modality": "minimized",
+            "location": "Monitor2"
+        },
+        {
+            "name": "Notion",
+            "action": "your_action_here",
+            "modality": "minimized",
+            "location": "Monitor2"
+        },
+        {
+            "name": "Safari",
+            "action": "your_action_here",
+            "modality": "minimized",
+            "location": "Monitor1",
+            "urls": [
+                {
+                    "url": "https://lsf.uni-heidelberg.de",
+                    "action": "your_action_here"
+                },
+                {
+                    "url": "https://moodle.uni-heidelberg.de/login/index.php",
+                    "action": "your_action_here"
+                }
+            ]
+        }
+    ]
+  }
+  
+  // Read URLs from a file and open in Safari
+  // var app = Application.currentApplication();
+  // app.includeStandardAdditions = true;
+  // var fileContents = app.read(
+  //   Path("/Users/<yourname>/<your-path>/config.json")
+  // );
+  // var config = JSON.parse(fileContents);
+  
+  // Open apps
+  config.apps.forEach(function (appConfig) {
+    var app = Application(appConfig.name);
+    app.activate();
+  
+    // Check if app is safari
+    if (appConfig.name == "Safari") {
+      // Open URLs in Safari
+      console.log("Opening URLs in Safari, urls: " + appConfig.urls);
+      handleSafari(appConfig, app);
+    }
+  
+    // Perform action based on appConfig.action
+    // Handle modality based on appConfig.modality
+    const modality = appConfig.modality;
+    if (modality) {
+      try {
+        if (modality == "fullscreen") {
+          app.windows[0].fullscreen = true;
+        } else if (modality == "minimized") {
+          app.windows[0].minimized = true;
+        } else if (modality == "hidden") {
+          app.windows[0].visible = false;
+        }
+      } catch (error) {
+        console.log("Error handling modality: " + error);
+      }
+    }
+  
+    // Handle location based on appConfig.location
+    const location = appConfig.location;
+    // Check on which screen the app should be opened
+    if (location) {
+      try {
+        if (location == "left") {
+          app.windows[0].bounds = { x: 0, y: 0, width: 960, height: 1080 };
+        } else if (location == "right") {
+          app.windows[0].bounds = { x: 960, y: 0, width: 960, height: 1080 };
+        }
+      } catch (error) {
+        console.log("Error handling location: " + error);
+      }
+    }
+  });
+  
+  function handleSafari(appConfig, safari) {
+    // Check if a Safari window is open
+    if (safari.windows.length === 0) {
+      // If not, open a new window
+      safari.Document().make();
+    }
+  
+    config = appConfig.urls;
+  
+    config.forEach(function (item) {
+      // Open URL in new tab
+      var currentWindow = safari.windows[0];
+      var newTab = safari.Tab({ url: item.url });
+      currentWindow.tabs.push(newTab);
+      currentWindow.currentTab = newTab;
+  
+      // Perform action based on item.action
+    });
+  }
+  
